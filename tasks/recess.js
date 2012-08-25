a81c8d90e59780fd4a4583976fa58b6e36db4242
Sindre Sorhus <sindresorhus@gmail.com>
2012-08-25 21:24:34 +0200

    Code style changes

--- tasks/recess.js
+++ tasks/recess.js
@@ -1,31 +1,24 @@
-/*
- * grunt-recess
- * 0.1.0 - 2012-05-06
- * github.com/sindresorhus/grunt-recess
- *
- * (c) Sindre Sorhus
- * sindresorhus.com
- * MIT License
- */
 module.exports = function( grunt ) {
 	'use strict';
 
 	grunt.registerMultiTask('recess', 'Grunt plugin to lint and minify CSS or LESS.', function() {
-		var recess = require('recess'),
-			lf = grunt.utils.linefeed,
-			done = this.async(),
-			files = grunt.file.expandFiles( this.file.src ),
-			dest = this.file.dest,
-			options = this.data.options || {},
-			compress = options.compress,
-			separator = compress ? '' : lf + lf;
+		var recess = require('recess');
+		var lf = grunt.utils.linefeed;
+		var cb = this.async();
+		var files = grunt.file.expandFiles( this.file.src );
+		var dest = this.file.dest;
+		var options = this.data.options || {};
+		var compress = options.compress;
+		var separator = compress ? '' : lf + lf;
 
 		recess( files, options, function( err, data ) {
-			var min = [],
-				max = [];
+			var min = [];
+			var max = [];
 
 			// RECESS returns an object when passed a single file,
-			// and a array of objects when passed multiple files
+			// and a array of objects when passed multiple files.
+			// ^ Bug: https://github.com/twitter/recess/issues/44
+			//
 			// .reverse() the array because of bug:
 			// https://github.com/twitter/recess/issues/43
 			data = Array.isArray( data ) ? data.reverse() : [ data ];
@@ -43,10 +36,10 @@ module.exports = function( grunt ) {
 					min.push( item.output );
 					max.push( item.data );
 				// Extract status and check
-				} else if ( item.output[1].indexOf('Perfect!') === -1 ) {
-					grunt.fail.warn( lf + item.output.join( lf ) );
-				} else {
+				} else if ( item.output[1].indexOf('Perfect!') !== -1 ) {
 					grunt.log.writeln( item.output.join( lf ) );
+				} else {
+					grunt.fail.warn( lf + item.output.join( lf ) );
 				}
 			});
 
@@ -55,6 +48,7 @@ module.exports = function( grunt ) {
 					// Concat files
 					grunt.file.write( dest, min.join( separator ) );
 					grunt.log.writeln( 'File "' + dest + '" created.' );
+
 					if ( compress ) {
 						grunt.helper( 'min_max_info', min.join( separator ), max.join( separator ) );
 					}
@@ -63,8 +57,7 @@ module.exports = function( grunt ) {
 				}
 			}
 
-			done();
+			cb();
 		});
 	});
-
 };
